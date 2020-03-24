ff6f111994802153d685e6a3247ffc68694bdfaa
hatuncev <hatuncev.ku@kvant-telecom.ru>
2020-03-25 00:14:31 +0300

    added example sources

---
+++ src/main/java/ru/home/structural/bridge/remotes/AdvancedRemote.java
@@ -0,0 +1,15 @@
+package ru.home.structural.bridge.remotes;
+
+import ru.home.structural.bridge.devices.Device;
+
+public class AdvancedRemote extends BasicRemote {
+
+    public AdvancedRemote(Device device) {
+        super.device = device;
+    }
+
+    public void mute() {
+        System.out.println("Remote: mute");
+        device.setVolume(0);
+    }
+}
---
+++ src/main/java/ru/home/structural/bridge/remotes/Remote.java
@@ -0,0 +1,13 @@
+package ru.home.structural.bridge.remotes;
+
+public interface Remote {
+    void power();
+
+    void volumeDown();
+
+    void volumeUp();
+
+    void channelDown();
+
+    void channelUp();
+}
